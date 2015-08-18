44afbc5e0d69b974cdb3b016e819cf1b1c3cc974
Yoshi <hiroluk@gmail.com>
2015-08-17 18:33:09 -0700

    form-based workflow feature completed

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,8 @@
 module ArticlesHelper
+  
+  #takes params hash from page, allows to take the whole "article hash"
+  # then permits only the title and body hash to be taken
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
